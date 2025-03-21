44093781b18bfd1d9c39061a2758a3b4d41e53c4
Hassan Belaqdour <hassanbelaqdour@gmail.com>
2025-03-21 15:13:04 +0000

    probleme de contenue de table

--- customer/database/migrations/2025_03_21_134330_create_responses_table.php
+++ customer/database/migrations/2025_03_21_134330_create_responses_table.php
@@ -1,36 +1,27 @@
 <?php
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
-     * @return void
      */
-    public function up()
+    public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id(); // Auto-incrementing primary key
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('responses', function (Blueprint $table) {
+            $table->id();
             $table->timestamps();
-            $table->enum('role', ['agent', 'client'])->default('client');
         });
     }
 
     /**
      * Reverse the migrations.
-     *
-     * @return void
      */
-    public function down()
+    public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('responses');
     }
-}
+};
--- customer/database/migrations/2025_03_21_150559_create_users_table.php
+++ customer/database/migrations/2025_03_21_150559_create_users_table.php
@@ -1,27 +1,36 @@
 <?php
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id(); // Auto-incrementing primary key
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
+            $table->enum('role', ['agent', 'client'])->default('client');
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('users');
     }
-};
+}
