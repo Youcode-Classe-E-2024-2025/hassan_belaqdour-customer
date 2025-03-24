826b71bfb450450cd84252f51e872320defcfdcc
Hassan Belaqdour <hassanbelaqdour@gmail.com>
2025-03-24 11:24:25 +0000

    ajout du modèle response avec relations

--- customer/app/Models/Response.php
+++ customer/app/Models/Response.php
@@ -2,9 +2,26 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Response extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'ticket_id',
+        'user_id',
+        'content',
+    ];
+
+    public function ticket()
+    {
+        return $this->belongsTo(Ticket::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
