1c2f8f6fd7f5494a6a135d2d31b9f2f6f425f1b0
Hassan Belaqdour <hassanbelaqdour@gmail.com>
2025-03-24 11:36:39 +0000

    ajout des routes pour les responses avec apiResource

--- customer/routes/api.php
+++ customer/routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
 use App\Http\Controllers\TicketController;
+use App\Http\Controllers\ResponseController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('tickets', TicketController::class);
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('tickets.responses', ResponseController::class)->shallow();
 });
