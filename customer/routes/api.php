d9ea1966d6e797e5770c50cfeb190dfa77b9368a
Hassan Belaqdour <hassanbelaqdour@gmail.com>
2025-03-25 10:12:02 +0000

    modifier routes pour quel soit proteger bien par laravel sanctum

--- customer/routes/api.php
+++ customer/routes/api.php
@@ -1,10 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\TicketController;
 use App\Http\Controllers\Api\ResponseController;
-use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function () {
+Route::middleware(['auth:sanctum'])->group(function () {
     Route::apiResource('tickets', TicketController::class);
     Route::apiResource('tickets.responses', ResponseController::class)->shallow();
 });
