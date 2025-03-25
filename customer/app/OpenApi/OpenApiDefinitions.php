f76b10062c10d4ba19413c586abe481cfda09c2c
Hassan Belaqdour <hassanbelaqdour@gmail.com>
2025-03-25 10:09:39 +0000

    modifier le chemin dans l5-swagger.php pour quel me derige vers app/http/controllers/api

--- customer/app/OpenApi/OpenApiDefinitions.php
+++ customer/app/OpenApi/OpenApiDefinitions.php
@@ -2,7 +2,14 @@
 
 namespace App\OpenApi;
 
+/**
+ * @OA\SecurityScheme(
+ *     securityScheme="sanctum",
+ *     type="http",
+ *     scheme="bearer",
+ *     bearerFormat="sanctum"
+ * )
+ */
 class OpenApiDefinitions
 {
-    
 }
